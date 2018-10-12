e3ff583001632d119a4208694c86bc130004aa40
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-10-12 22:12:26 +0800

    sync on windows

---
+++ Assets/Scripts/ArrayListTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class ArrayListTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(Thread.CurrentThread.ManagedThreadId);
+		Debug.Log(Thread.CurrentThread.Name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
